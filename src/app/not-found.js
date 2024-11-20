ff93131d4da65d7c598909eb8601f645d4135535
Fred Rukundo <dukefred9@gmail.com>
2024-11-20 11:38:54 +0100

    not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
 
-
 export default function NotFound() {
     return (
-        <div>
-            <h1>Not Found page</h1>
-            <Link href={"/"}>Go back Home</Link>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
+            <p className="text-xl text-gray-600 mb-6">Oops! The page you're looking for doesn't exist.</p>
+            <Link href="/">
+                <h2 className="px-6 py-3 bg-blue-500 text-white font-medium rounded-lg shadow-lg hover:bg-blue-600 transition-all duration-300">
+                    Go back Home
+                </h2>
+            </Link>
         </div>
     );
 }
