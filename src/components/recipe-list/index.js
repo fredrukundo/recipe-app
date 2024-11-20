f7687a444675a4a7cf03edc3b96a2ddd0f205488
Fred Rukundo <dukefred9@gmail.com>
2024-11-20 11:40:45 +0100

    recipe lists

--- src/components/recipe-list/index.js
+++ src/components/recipe-list/index.js
@@ -1,53 +1,59 @@
 import {
     Card,
     CardContent,
-  } from "@/components/ui/card"
+} from "@/components/ui/card"
 import Link from "next/link";
 
-export default function RecipeList({recipeList}) {
+export default function RecipeList({ recipeList }) {
     console.log("recipe component")
     console.log(recipeList);
     return (
         <div>
-           <div className="p-4 mx-auto lg:max-w-6xl md:max-w-4xl sm:max-w-full">
-            <h2 className="text-4xl font-bold text-gray-800 mb-12">Recipes</h2>
-            <Link href="/">Go Home</Link>
-            
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                { recipeList && recipeList.length > 0 ?
-                recipeList.map((recipe) => (
-                    <Link href={`/recipe-list/${recipe.id}`}>
-                        <Card>
-                            <CardContent className="bg-white rounded-md overflow-hidden shadow-md cursor-pointer hover:scale-[1.1] transition-all">
-                                <div className="w-full aspect-w-16 aspect-h-8 lg:h-80">
-                                    <img 
-                                    src={recipe.image}
-                                    alt={recipe.name}
-                                    className="h-full w-full object-cover object-top"
-                                    />
-                                </div>
-                                <div className="p-6">
-                                    <h3 className="text-lg font-bold text-gray-900">
-                                        {recipe.name}
-                                    </h3>
-                                    <div className="mt-4 flex items-center flex-wrap gap-2">
-                                        <p className="text-lg text-gray-600">
-                                            Rating: {recipe.rating}
-                                        </p>
-                                        <div className="ml-auto">
-                                            <p className="text-lg text-gray-600 font-bold">
-                                                {recipe.cuisine}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </CardContent>
-                        </Card>
-                    </Link>
-                )) : null}
+            <div className="p-4 mx-auto lg:max-w-6xl md:max-w-4xl sm:max-w-full">
+                <h2 className="text-4xl font-bold text-gray-800 mb-12">Recipes</h2>
+                <div className="pb-16 mx-auto">
+                <Link href="/" >
+                <h2 className="inline-block px-4 py-2 bg-blue-500 text-sm text-white font-medium rounded-lg shadow-lg hover:bg-blue-600 transition-all duration-300">
+                    Go Home
+                </h2>
+                </Link>
+                </div>
 
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {recipeList && recipeList.length > 0 ?
+                        recipeList.map((recipe) => (
+                            <Link href={`/recipe-list/${recipe.id}`}>
+                                <Card>
+                                    <CardContent className="bg-white rounded-md overflow-hidden shadow-md cursor-pointer hover:scale-[1.1] transition-all">
+                                        <div className="w-full aspect-w-16 aspect-h-8 lg:h-80">
+                                            <img
+                                                src={recipe.image}
+                                                alt={recipe.name}
+                                                className="h-full w-full object-cover object-top"
+                                            />
+                                        </div>
+                                        <div className="p-6">
+                                            <h3 className="text-lg font-bold text-gray-900">
+                                                {recipe.name}
+                                            </h3>
+                                            <div className="mt-4 flex items-center flex-wrap gap-2">
+                                                <p className="text-lg text-gray-600">
+                                                    Rating: {recipe.rating}
+                                                </p>
+                                                <div className="ml-auto">
+                                                    <p className="text-lg text-gray-600 font-bold">
+                                                        {recipe.cuisine}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </Link>
+                        )) : null}
+
+                </div>
             </div>
-           </div>
         </div>
     );
 }
