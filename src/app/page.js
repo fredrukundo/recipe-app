4129a192194db8713540dd826dd45fb5c0b647da
Fred Rukundo <dukefred9@gmail.com>
2024-11-20 11:39:20 +0100

    Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
 import Link from "next/link";
 
-
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the recipe App</h1>
-      <Link href={"/recipe-list"}>Explore</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-green-400 via-yellow-300 to-orange-500">
+      <h1 className="text-4xl font-bold text-white mb-6">
+        Welcome to the <span className="text-yellow-200">Recipe App</span>
+      </h1>
+      <Link href="/recipe-list">
+        <h2 className="px-6 py-3 bg-white text-green-600 font-medium rounded-lg shadow-lg hover:bg-yellow-200 hover:text-green-800 transition-all duration-300">
+          Explore The Recipes
+        </h2>
+      </Link>
     </div>
   );
 }
+
